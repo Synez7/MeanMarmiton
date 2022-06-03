326509f89f1da2a209289303dc6bcd11656b4452
Synez7 <diesel140@hotmail>
2022-06-03 21:20:18 +0200

    delete project

--- MARMITON_SERVEUR/script.sh
+++
@@ -1,9 +0,0 @@
-mongoimport --db MARMITON --collection ingredients --file ./Collections/ingredients.json --jsonArray --drop
-mongoimport --db MARMITON --collection recettes --file ./Collections/recettes.json --jsonArray --drop
-mongoimport --db MARMITON --collection users --file ./Collections/users.json --jsonArray --drop
-mongoimport --db MARMITON --collection commentaire --file ./Collections/commentaire.json --jsonArray --drop
-
-node serveur.js
-
-
-
--- run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-gnome-terminal -- /bin/sh -c 'cd ./MARMITON_SERVEUR; npm i;  ./script.sh'
-
-gnome-terminal -- /bin/sh -c 'cd ./MARMITON_CLIENT/; npm i; cd ./src/app; ng serve -o'
-
